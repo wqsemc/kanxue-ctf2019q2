a341a8eb132753f931d10a19ba0663f332480164
semcwang <36132717+wqsemc@users.noreply.github.com>
2019-09-07 11:45:45 +0800

    Create kanxue-ctf2019q2-topic6.c

---
+++ kanxue-ctf2019q2-topic6.c
@@ -0,0 +1,13 @@
+v3 = strlen((const char *)&bindata);
+
+base64_encode(&bindata, v7, v3);
+
+v6 = "!NGV%,$h1f4S3%2P(hkQ94==";
+
+if ( !strcmp("!NGV%,$h1f4S3%2P(hkQ94==", v7) )
+
+puts("Success");
+
+else
+
+puts("Please Try Again");
